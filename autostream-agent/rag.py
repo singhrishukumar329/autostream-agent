87fce4c9456b4b4b3af724f74a7a69d7dc945fdc
singhrishukumar329 <singhrishukumar329@gmail.com>
2026-04-23 14:31:08 +0530

    Delete autostream-agent directory

--- autostream-agent/rag.py
+++
@@ -1,7 +0,0 @@
-def get_knowledge():
-    with open("knowledge.txt", "r") as f:
-        return f.read()
-
-def retrieve_answer(query):
-    data = get_knowledge()
-    return data
--- autostream-agent/tools.py
+++
@@ -1,2 +0,0 @@
-def mock_lead_capture(user_input):
-    return f"Lead captured for query: {user_input}"
